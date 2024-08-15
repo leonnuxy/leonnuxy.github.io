8bc21557538831164d46860391872ecb7cfee5fb
Noel <1leonnoel1@gmail.com>
2024-08-15 14:34:47 -0600

    deleted base.html

---
+++ bookstore/js/script.js
@@ -0,0 +1,7 @@
+function openSlider() {
+    document.getElementById("signin-slider").classList.add("active");
+}
+
+function closeSlider() {
+    document.getElementById("signin-slider").classList.remove("active");
+}
