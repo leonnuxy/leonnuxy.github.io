43918c514a5e5e14a9761570c7d398bfc781ef88
Noel <1leonnoel1@gmail.com>
2024-08-16 15:04:47 -0600

    deleted base.html

--- bookstore/js/script.js
+++ bookstore/js/script.js
@@ -1,18 +1,16 @@
 
-// Sign In Popup
-function openPopup() {
-    document.getElementById("signin-popup").style.display = "block";
+function openSlider(sliderId) {
+    document.getElementById(sliderId).classList.add('active');
 }
 
-function closePopup() {
-    document.getElementById("signin-popup").style.display = "none";
+function closeSlider(sliderId) {
+    document.getElementById(sliderId).classList.remove('active');
 }
 
-// Cart Slider 
 function openPopup(popupId) {
-    document.getElementById(popupId).style.display = "block";
+    document.getElementById(popupId).style.display = 'block';
 }
 
 function closePopup(popupId) {
-    document.getElementById(popupId).style.display = "none";
+    document.getElementById(popupId).style.display = 'none';
 }
